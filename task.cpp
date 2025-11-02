54f559017ebb16fb88a343596b4d0f169662f39b
ranaemiii132-coder <ranaemiii132@gmail.com>
2025-11-02 21:36:21 +0500

    Add files via upload

---
+++ task.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string num = "eman";
+    char x = (char) num;   // manually convert double to int
+    cout<<"Converted value: "<<x;
+}
+
